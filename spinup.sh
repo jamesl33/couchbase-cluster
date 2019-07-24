085f16208e0e5bf92b9eddec92086cdbcca882c7
James Lee <jamesl33info@gmail.com>
2019-07-24 21:45:10 +0100

    Improve default provisioning and allow the creation of 0 < n > 4 nodes

--- spinup.sh
+++ spinup.sh
@@ -6,11 +6,23 @@ if [[ $USER != 'root' ]]; then
     echo 'Error: This script must be run as root' 1>&2 && exit 1
 fi
 
+num_nodes=$1
+
+if [[ $num_nodes == 0 || $num_nodes > 4 ]]; then
+    num_nodes=4
+fi
+
 if [[ -z $CLUSTER_UUID ]]; then
     CLUSTER_UUID=$(uuidgen)
 fi
 
-CLUSTER_UUID=$CLUSTER_UUID docker-compose up -d "$@"
+nodes=""
+
+for i in $(seq $num_nodes); do
+    nodes="$nodes node$i"
+done
+
+CLUSTER_UUID=$CLUSTER_UUID docker-compose up -d $nodes
 
 function cleanup() {
     docker-compose stop
@@ -22,14 +34,37 @@ trap cleanup SIGINT
 
 sleep 15 # wait for the couchbase nodes to initialise
 
-curl -s -X POST http://172.20.1.1:8091/pools/default -d 'memoryQuota=512' -d 'indexMemoryQuota=256' -d 'ftsMemoryQuota=256' -d 'cbasMemoryQuota=1024' -d 'eventingMemoryQuota=256' > /dev/null
-curl -s -X POST http://172.20.1.1:8091/node/controller/setupServices -d 'services=kv' > /dev/null
-curl -s -X POST http://172.20.1.1:8091/settings/web -d 'username=admin&password=password&port=SAME' > /dev/null
-curl -s -u admin:password 172.20.1.1:8091/settings/indexes -d 'storageMode=plasma' > /dev/null
-curl -s -u admin:password 172.20.1.1:8091/controller/addNode -d 'hostname=172.20.1.2' -d 'user=admin' -d 'password=password' -d 'services=kv' > /dev/null
-curl -s -u admin:password 172.20.1.1:8091/controller/addNode -d 'hostname=172.20.1.3' -d 'user=admin' -d 'password=password' -d 'services=kv' > /dev/null
-curl -s -u admin:password 172.20.1.1:8091/controller/addNode -d 'hostname=172.20.1.4' -d 'user=admin' -d 'password=password' -d 'services=index%2Cfts%2Cn1ql%2Ccbas%2Ceventing' > /dev/null
-curl -s -u admin:password 172.20.1.1:8091/controller/rebalance -d 'knownNodes=ns_1%40172.20.1.1%2Cns_1%40172.20.1.2%2Cns_1%40172.20.1.3%2Cns_1%40172.20.1.4' > /dev/null
+if [[ $num_nodes == 1 ]]; then
+    curl -s -X POST http://172.20.1.1:8091/settings/web -d 'username=admin&password=password&port=SAME' > /dev/null
+    curl -s -u admin:password http://172.20.1.1:8091/pools/default -d 'memoryQuota=512' -d 'indexMemoryQuota=256' -d 'ftsMemoryQuota=256' -d 'cbasMemoryQuota=1024' -d 'eventingMemoryQuota=256' > /dev/null
+    curl -s -u admin:password http://172.20.1.1:8091/settings/indexes -d 'storageMode=plasma' > /dev/null
+    curl -s -u admin:password http://172.20.1.1:8091/node/controller/setupServices -d 'services=kv%2Cindex%2Cfts%2Cn1ql%2Ccbas%2Ceventing' > /dev/null
+else
+    curl -s -X POST http://172.20.1.1:8091/settings/web -d 'username=admin&password=password&port=SAME' > /dev/null
+    curl -s -u admin:password http://172.20.1.1:8091/pools/default -d 'memoryQuota=512' -d 'indexMemoryQuota=256' -d 'ftsMemoryQuota=256' -d 'cbasMemoryQuota=1024' -d 'eventingMemoryQuota=256' > /dev/null
+    curl -s -u admin:password http://172.20.1.1:8091/settings/indexes -d 'storageMode=plasma' > /dev/null
+    curl -s -u admin:password http://172.20.1.1:8091/node/controller/setupServices -d 'services=kv' > /dev/null
+
+    for node in $(seq 2 $num_nodes); do
+        if [[ $node == $num_nodes ]]; then
+            curl -s -u admin:password 172.20.1.1:8091/controller/addNode -d "hostname=172.20.1.$node" -d 'user=admin' -d 'password=password' -d 'services=index%2Cfts%2Cn1ql%2Ccbas%2Ceventing' > /dev/null
+        else
+            curl -s -u admin:password 172.20.1.1:8091/controller/addNode -d "hostname=172.20.1.$node" -d 'user=admin' -d 'password=password' -d 'services=kv' > /dev/null
+        fi
+    done
+fi
+
+known_nodes=""
+
+for node in $(seq $num_nodes); do
+    known_nodes+="ns_1%40172.20.1.$node"
+
+    if [[ $node != $num_nodes ]]; then
+        known_nodes+="%2C"
+    fi
+done
+
+curl -s -u admin:password 172.20.1.1:8091/controller/rebalance -d "knownNodes=$known_nodes" > /dev/null
 
 while true; do
     docker wait $(docker-compose ps -q | tr '\n' ' ') &> /dev/null
