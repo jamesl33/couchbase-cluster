64bdf3158473094325366a9a6af6caf4e77dc719
James Lee <james.lee@couchbase.com>
2023-02-08 10:30:26 +0000

    Update 'spinup.sh' script
    1. Pin version to 7.1.3
    2. Add IPV6 support (disabled by default)
    3. Change username/password
    4. Add the backup service to the final node
    5. Switch to using 'clusterInit' endpoint
    6. Remove 'docker rm' command that was failing

--- spinup.sh
+++ spinup.sh
@@ -26,7 +26,6 @@ CLUSTER_UUID=$CLUSTER_UUID docker-compose up -d $nodes
 
 function cleanup() {
     docker-compose stop
-    docker rm $(docker-compose ps -q | tr '\n' ' ') > /dev/null
     rm -rf /tmp/couchbase-cluster-$CLUSTER_UUID > /dev/null
 }
 
@@ -34,22 +33,18 @@ trap cleanup SIGINT
 
 sleep 15 # wait for the couchbase nodes to initialise
 
-if [[ $num_nodes == 1 ]]; then
-    curl -s -X POST http://172.20.1.1:8091/settings/web -d 'username=admin&password=password&port=SAME' > /dev/null
-    curl -s -u admin:password http://172.20.1.1:8091/pools/default -d 'memoryQuota=512' -d 'indexMemoryQuota=256' -d 'ftsMemoryQuota=256' -d 'cbasMemoryQuota=1024' -d 'eventingMemoryQuota=256' > /dev/null
-    curl -s -u admin:password http://172.20.1.1:8091/settings/indexes -d 'storageMode=plasma' > /dev/null
-    curl -s -u admin:password http://172.20.1.1:8091/node/controller/setupServices -d 'services=kv%2Cindex%2Cfts%2Cn1ql%2Ccbas%2Ceventing' > /dev/null
+if [[ $num_nodes == "1" ]]; then
+    curl -s -X POST http://172.20.1.1:8091/clusterInit -d 'username=Administrator&password=asdasd&port=SAME&services=kv%2Cindex%2Cfts%2Cn1ql%2Ccbas%2Ceventing%2Cbackup&memoryQuota=512&indexMemoryQuota=256&ftsMemoryQuota=256&cbasMemoryQuota=1024&eventingMemoryQuota=256' > /dev/null
+    curl -s -u Administrator:asdasd http://172.20.1.1:8091/settings/indexes -d 'storageMode=plasma' > /dev/null
 else
-    curl -s -X POST http://172.20.1.1:8091/settings/web -d 'username=admin&password=password&port=SAME' > /dev/null
-    curl -s -u admin:password http://172.20.1.1:8091/pools/default -d 'memoryQuota=512' -d 'indexMemoryQuota=256' -d 'ftsMemoryQuota=256' -d 'cbasMemoryQuota=1024' -d 'eventingMemoryQuota=256' > /dev/null
-    curl -s -u admin:password http://172.20.1.1:8091/settings/indexes -d 'storageMode=plasma' > /dev/null
-    curl -s -u admin:password http://172.20.1.1:8091/node/controller/setupServices -d 'services=kv' > /dev/null
+    curl -s -X POST http://172.20.1.1:8091/clusterInit -d 'username=Administrator&password=asdasd&port=SAME&services=kv&memoryQuota=512&indexMemoryQuota=256&ftsMemoryQuota=256&cbasMemoryQuota=1024&eventingMemoryQuota=256' > /dev/null
+    curl -s -u Administrator:asdasd http://172.20.1.1:8091/settings/indexes -d 'storageMode=plasma' > /dev/null
 
     for node in $(seq 2 $num_nodes); do
         if [[ $node == $num_nodes ]]; then
-            curl -s -u admin:password 172.20.1.1:8091/controller/addNode -d "hostname=172.20.1.$node" -d 'user=admin' -d 'password=password' -d 'services=index%2Cfts%2Cn1ql%2Ccbas%2Ceventing' > /dev/null
+            curl -s -u Administrator:asdasd 172.20.1.1:8091/controller/addNode -d "hostname=172.20.1.$node" -d 'user=Administrator' -d 'password=asdasd' -d 'services=index%2Cfts%2Cn1ql%2Ccbas%2Ceventing%2Cbackup' > /dev/null
         else
-            curl -s -u admin:password 172.20.1.1:8091/controller/addNode -d "hostname=172.20.1.$node" -d 'user=admin' -d 'password=password' -d 'services=kv' > /dev/null
+            curl -s -u Administrator:asdasd 172.20.1.1:8091/controller/addNode -d "hostname=172.20.1.$node" -d 'user=Administrator' -d 'password=asdasd' -d 'services=kv' > /dev/null
         fi
     done
 fi
@@ -64,7 +59,7 @@ for node in $(seq $num_nodes); do
     fi
 done
 
-curl -s -u admin:password 172.20.1.1:8091/controller/rebalance -d "knownNodes=$known_nodes" > /dev/null
+curl -s -u Administrator:asdasd 172.20.1.1:8091/controller/rebalance -d "knownNodes=$known_nodes" > /dev/null
 
 while true; do
     docker wait $(docker-compose ps -q | tr '\n' ' ') &> /dev/null
